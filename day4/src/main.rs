117e2d16b1660e89f4e91ebee2d498bad34805da
Guillaume Mantopoulos <3082385+MrGuiMan@users.noreply.github.com>
2023-12-04 12:57:44 +0100

    day4 completion

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn main() -> std::io::Result<()> {
@@ -6,6 +8,7 @@ fn main() -> std::io::Result<()> {
         println!("----");
         println!("Day 4");
         println!("Part 1: {}", compute_answer_1(&contents));
+        println!("Part 2: {}", compute_answer_2(&contents));
         println!("----");
     }
     Ok(())
@@ -13,31 +16,80 @@ fn main() -> std::io::Result<()> {
 
 fn compute_answer_1(contents: &str) -> String {
     format!("{}", contents.lines()
-            .map(|line| {
-                println!("{}", line);
-                if let Some((_, number_lists)) = line.split_once(':') {
-                    return get_matches_score(number_lists)
-                }
-                0
-            })
-            .sum::<usize>())
+        .map(Card::from_str)
+        .filter_map(Result::ok)
+        .map(Card::calculate_score)
+        .sum::<usize>())
 }
 
-fn get_matches_score(number_lists: &str) -> usize {
-    if let Some((winning_numbers_str, owned_numbers_str)) = number_lists.split_once('|') {
-        let (winning_numbers, owned_numbers) = (parse_numbers(winning_numbers_str), parse_numbers(owned_numbers_str));
-
-        let matches: Vec<&usize> = winning_numbers.intersection(&owned_numbers).collect();
-        if matches.len() >= 1 {
-            return 1 * ((2 as usize).pow((matches.len().saturating_sub(1)) as u32))
-        }
-    }
-    0
+fn compute_answer_2(contents: &str) -> String {
+    format!("{}", contents.lines()
+        .map(Card::from_str)
+        .filter_map(Result::ok)
+        .fold(HashMap::new(), |mut card_counts, card| {
+            // Scorecards are "copied" by means of increasing count by how many of the current card
+            // we have
+            let current_scorecard_count = *card_counts.entry(card.id).and_modify(|x| { *x += 1 }).or_insert(1);
+            for j in 1..= card.get_matches().len() {
+                card_counts
+                    .entry(card.id + j)
+                    .and_modify(|count| { *count += current_scorecard_count;})
+                    .or_insert(current_scorecard_count);
+            }
+            card_counts
+        })
+        .values().sum::<usize>())
 }
-
 fn parse_numbers(number_list: &str) -> HashSet<usize> {
-    number_list.trim().split_whitespace()
+    number_list.split_whitespace()
         .map(str::parse::<usize>)
         .filter_map(Result::ok)
         .collect()
+}
+
+struct Card {
+    id: usize,
+    winning_numbers: HashSet<usize>,
+    draw_numbers: HashSet<usize>
+}
+impl Card {
+    pub fn calculate_score(card: Card) -> usize{
+        card.score()
+    }
+    fn get_matches(&self) -> HashSet<&usize> {
+        self.winning_numbers.intersection(&self.draw_numbers).collect()
+    }
+    fn score(&self)  -> usize {
+        match self.get_matches().len() {
+            match_count if match_count > 0 => 2usize.pow(match_count.saturating_sub(1) as u32),
+            _ => 0
+        }
+    }
+}
+impl FromStr for Card {
+    type Err = ();
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        if let Some((card_str, numbers_str)) = string.split_once(':') {
+            let all_numbers = numbers_str.split_once('|')
+                .map(|(win,draw)| (parse_numbers(win), parse_numbers(draw)));
+
+            let card_number = card_str.split_whitespace().last()
+                .map(str::parse::<usize>)
+                .and_then(Result::ok);
+
+            return match (card_number, all_numbers) {
+                (Some(id), Some((winning_numbers, draw_numbers))) => Ok(
+                    Card {
+                        id,
+                        winning_numbers,
+                        draw_numbers
+                    }
+                    ),
+                _ => Err(())
+            }
+        }
+
+        Err(())
+    }
 }
